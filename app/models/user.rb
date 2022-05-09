620ae16c920a010eb1d7b80a1efc401852fe9952
malvart <102654106+malvart@users.noreply.github.com>
2022-05-09 18:22:36 +0900

    deviseの導入からマイグレートまで

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  with_options uniqueness: true do
+    validates :nickname, presence: true
+    validates :email
+  end
+
+end
