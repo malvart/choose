d576b6703391a6d100f2e6c4db24eaff9624db0c
malvart <102654106+malvart@users.noreply.github.com>
2022-05-20 18:34:42 +0900

    完成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,16 +2,18 @@ class FavoritesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @cooking = Cooking.find(params[:id])
-    if current_user != @cooking.user
+    @cooking = Cooking.find(params[:cooking_id])
+    if current_user.id != @cooking.user_id
       @favorite = Favorite.create(user_id: current_user.id, cooking_id: @cooking.id)
     end
+    redirect_back fallback_location: cooking_path(@cooking.id)
   end
 
   def destroy
-    @cooking = Cooking.find(params[:id])
+    @cooking = Cooking.find(params[:cooking_id])
     @favorite = Favorite.find_by(user_id: current_user.id, cooking_id: @cooking.id)
     @favorite.destroy
+    redirect_back fallback_location: cooking_path(@cooking.id)
   end
 
 end
