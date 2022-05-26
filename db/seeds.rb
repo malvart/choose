9a6d99f99b35ae4124adbd2dcecbf17e043f4d77
malvart <102654106+malvart@users.noreply.github.com>
2022-05-26 11:29:07 +0900

    seed設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,27 @@ cats = [
 cats.each do |cat|
   Category.create!(category_name: "#{cat}")
 end
+
+User.create!(
+  nickname: "テスト"
+  email: "test@test"
+  password: "123qwe"
+)
+
+cookings = [
+  "チャーハン", "ハンバーグ", "シーフードサラダ", "野菜スムージー"
+]
+
+cookings.each do |cooking|
+  Cooking.create!(
+    cooking_name: "#{cooking}"
+    user_id: 1
+  )
+end
+
+3.times do |n|
+  CookingCategory.create!(
+    cooking_id: n + 1
+    category_id: n + 1
+  )
+end
