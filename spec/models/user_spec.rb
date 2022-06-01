ec4095353ee099c5a44f92530a9606806da63452
malvart <102654106+malvart@users.noreply.github.com>
2022-06-01 12:07:28 +0900

    ユーザー新規登録の単体テストコード

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録できる場合' do #正常系
+      it 'nickname, email, password, password_confirmationが存在すれば登録できる' do
+        expect(@user).to be_valid
+      end
+    end
+    
+    context '新規登録できない場合' do #異常系
+      it 'nicknameが空では登録できない' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("ニックネームを入力してください")
+      end
+      it 'emailが空では登録できない' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Eメールを入力してください")
+      end
+      it 'passwordが空では登録できない' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("パスワードを入力してください", "パスワード（確認用）とパスワードの入力が一致しません")
+      end
+    end
+  end
+end
