10083dc2d1802615d8b2469af008baf91615d9ee
malvart <102654106+malvart@users.noreply.github.com>
2022-06-10 22:25:54 +0900

    FactoryBotの記述

--- spec/factories/cooking_categories.rb
+++ spec/factories/cooking_categories.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :cooking_category do
-    
+    association :cooking
+    association :category
   end
 end
--- spec/factories/cookings.rb
+++ spec/factories/cookings.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
   factory :cooking do
-    cooking_name
-    
+    sequence(:cooking_name) { |n| "cooking_name-#{n}" }
+
+    after(:create) do |cooking|
+      create_list(:cooking_category, 1, cooking: cooking, category: create(category))
+    end
   end
 end
