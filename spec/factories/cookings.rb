3ce3d2d3a9a8b2f1544a2615f8dccc4a12b3e38f
malvart <102654106+malvart@users.noreply.github.com>
2022-06-15 22:44:17 +0900

    正常系テスト

--- spec/factories/cookings.rb
+++ spec/factories/cookings.rb
@@ -1,6 +1,10 @@
 FactoryBot.define do
   factory :cooking do
     cooking_name {'ハンバーグ'}
-    
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('app/assets/images/no_image_yoko.jpg'), filename: 'no_image_yoko.jpg')
+    end
   end
 end
--- spec/models/cooking_spec.rb
+++ spec/models/cooking_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Cooking, type: :model do
-  let!(:cooking) { create(:cooking) }
+  before do
+    @cooking = FactoryBot.build(:cooking)
+  end
 
-  it "cooking_nameがある場合は登録できる" do
-    expect(cooking).to be_valid
+  describe "料理登録" do
+    context "料理を登録できる場合" do
+      it "cooking_nameがある場合は登録できる" do
+        expect(@cooking).to be_valid
+      end
+      it "imageはなくても登録できる" do
+        @cooking.image = nil
+        expect(@cooking).to be_valid
+      end
+    end
   end
 end
